110f0c31c6bcf3ae18f5224a87a19496de023334
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 13:37:50 +0530

    updated height

--- src/components/homepage/Link.jsx
+++ src/components/homepage/Link.jsx
@@ -4,15 +4,15 @@ function Link(){
     return(
         <div className="flex justify-center items-center">
             <div className="bg-[#343434] w-[1500px] flex h-[380px] rounded-xl m-28">
-                <div className="p-5 w-6/12">
+                <div className="p-4 w-6/12">
                     <Title
                     title="OpenType features and Variable fonts"/>
                     <a href="#"  className="font-medium">
                 <button type="button" className="btn w-60 h-14 bg-[#4452FE] text-white ml-5 hover:bg-white hover:text-[#4452FE]">Try For Free</button>
                 </a>
                 </div>
-                <div className="content-end">
-                    <img src="src\assets\template.png"></img>
+                <div className="content-end ">
+                    <img src="src\assets\template.png" className="h-full"></img>
                 </div>
             </div>
         </div>
