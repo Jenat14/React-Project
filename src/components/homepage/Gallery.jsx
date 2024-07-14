e546bb274659c0ee4495a4caf0dd4dd5dbf62b78
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 16:10:06 +0530

    Corrections in gallery

--- src/components/homepage/Gallery.jsx
+++ src/components/homepage/Gallery.jsx
@@ -7,29 +7,29 @@ function Gallery(){
             title="Gallery"
             description="We focus on ergonomics and meeting you where you work. It's only a keystroke away."/>
             <div className="grid-rows-1 w-4/5">
-            <div className="grid items-center grid-cols-1 m-5 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="grid w-full items-center grid-cols-1 m-5 sm:grid-cols-2 lg:grid-cols-4">
                 <div>
-                    <img src="src\assets\g1.png" alt="Image 1" className="w-full h-64 object-cover rounded-md"/>
+                    <img src="src\assets\g1.png" alt="Image 1" className="h-64 object-cover m-3 rounded-md"/>
                 </div>
                 <div>
-                    <img src="src\assets\g2.png" alt="Image 2" className="w-full h-64 object-cover rounded-md"/>
+                    <img src="src\assets\g2.png" alt="Image 2" className=" h-64 object-cover m-3 rounded-md"/>
                 </div>
                 <div>
-                    <img src="src\assets\g3.png" alt="Image 3" className="w-full h-64 object-cover rounded-md"/>
+                    <img src="src\assets\g3.png" alt="Image 3" className=" h-64 object-cover m-3 rounded-md"/>
                 </div>
                 <div>
-                    <img src="src\assets\g4.png" alt="Image 4" className="w-full h-64 object-cover rounded-md"/>
+                    <img src="src\assets\g4.png" alt="Image 4" className="h-64 object-cover m-3 rounded-md"/>
                 </div>
             </div>
-            <div className="grid m-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid w-full gap-5 items-center grid-cols-1 m-5 sm:grid-cols-2 lg:grid-cols-3">
                 <div>
-                    <img src="src\assets\g5.png" alt="Image 1" className="w-full h-64 object-cover rounded-lg"/>
+                    <img src="src\assets\g5.png" alt="Image 1" className="w-full h-64 object-cover m-3 rounded-md"/>
                 </div>
                 <div>
-                    <img src="src\assets\g6.png" alt="Image 2" className="w-full h-64 object-cover rounded-[50px]"/>
+                    <img src="src\assets\g6.png" alt="Image 2" className="w-full h-64 object-cover m-3 rounded-md"/>
                 </div>
                 <div>
-                    <img src="src\assets\g7.png" alt="Image 3" className="w-full h-64 object-cover rounded-[50px]"/>
+                    <img src="src\assets\g7.png" alt="Image 3" className="w-full h-64 object-cover m-3 rounded-md"/>
                 </div>
             </div>
             </div>
