47f600a82c3d86b9f9b44a616274b11e31855fc9
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 16:17:45 +0530

    margin

--- src/components/homepage/Photo.jsx
+++ src/components/homepage/Photo.jsx
@@ -1,8 +1,8 @@
 function Photo(){
     return(
-        <div className="relative mt-4 h-[800px]">
+        <div className="relative mt-4 mb-40">
             <img src="src\assets\i-bg.png" className="z-index[-10]"></img>
-            <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black"></div>
+            <div class="absolute top-20 left-0 w-full h-full bg-gradient-to-b from-transparent to-black"></div>
         </div>
     )
 }
