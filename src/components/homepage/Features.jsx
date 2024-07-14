82e62a78efadc321600a6cac09c4bcc21573394d
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 15:04:44 +0530

    Functions added

--- src/components/homepage/Features.jsx
+++ src/components/homepage/Features.jsx
@@ -1,7 +1,16 @@
 import Title from "./Title";
 function Features(){
+    const Content = ({icon,heading,text}) => (
+        <div className="flex flex-col m-3 mb-[70px]">
+            <div className="flex justify-space w-[190px] text-left">
+                <img src={icon} className="mr-3"></img>
+                <h4 className="content-center font-medium text-xl">{heading}</h4>
+            </div>
+            <p className=" font-normal text-base w-72 mt-2 text-left">{text}</p>
+        </div>
+    );
     return(
-        <div className=" h-auto flex flex-col items-center justify-center text-white text-center h-[1200px]">
+        <div className=" h-auto flex flex-col items-center justify-center text-white text-center h-[1200px] mb-40">
             <Title
                 title="Features"
                 description="Most calendars are designed for teams. Slate is designed for 
@@ -12,30 +21,21 @@ function Features(){
                     <img src="src/assets/features-chart.png" class="absolute top-[450px] left-[350px] z-20" alt="Chart Image"></img>
                 </div>
                 <div className="ml-11">
-                    <div className="flex flex-col ml-4 my-10 mb-[70px]">
-                        <div className="flex justify-space w-[190px] text-left">
-                            <img src="src\assets\f-round-restaurant-menu.png" className="mr-3"></img>
-                            <h4 className="content-center font-medium text-xl">A single source of truth</h4>
-                        </div>
-                        <p className=" font-normal text-base w-72 mt-2 text-left">When you add work to your Slate calendar we automatically calculate useful insights 
-                        </p>
-                    </div>
-                    <div className="flex flex-col m-3 mb-[70px]">
-                        <div className="flex justify-space w-[190px] text-left">
-                            <img src="src\assets\f-_alien-outline.png" className="mr-3"></img>
-                            <h4 className="content-center font-medium text-xl">Intuitive interface</h4>
-                        </div>
-                        <p className=" font-normal text-base w-72 mt-2 text-left">When you add work to your Slate calendar we automatically calculate useful insights 
-                        </p>
-                    </div>
-                    <div className="flex flex-col m-3 mb-[70px]">
-                        <div className="flex justify-space w-[190px] text-left">
-                            <img src="src\assets\f-_all-inclusive.png" className="mr-3"></img>
-                            <h4 className="content-center font-medium text-xl">Or with rules</h4>
-                        </div>
-                        <p className=" font-normal text-base w-72 mt-2 text-left">When you add work to your Slate calendar we automatically calculate useful insights 
-                        </p>
-                    </div>
+                    <Content
+                    icon="src\assets\f-round-restaurant-menu.png"
+                    heading="A single source of truth"
+                    text="When you add work to your Slate calendar we automatically calculate useful insights "
+                    />
+                    <Content
+                    icon="src\assets\f-_alien-outline.png"
+                    heading="Intuitive interface"
+                    text="When you add work to your Slate calendar we automatically calculate useful insights "
+                    />
+                    <Content
+                    icon="src\assets\f-_all-inclusive.png"
+                    heading="Or with rules"
+                    text="When you add work to your Slate calendar we automatically calculate useful insights "
+                    />
                 </div>
             </div>
         </div>
