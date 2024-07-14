8dd3dee55e9737281c0cdcebf077c1bcd2b08412
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 22:34:19 +0530

    Changes

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,11 +1,11 @@
 function Hero(){
     return(
         <div className="h-auto flex flex-col items-center justify-center text-white text-center">
-            <h1 className="mt-10 font-medium text-[80px] w-[700px]">Work at the speed of thought</h1>
-            <p className="p my-5 w-[600px] font-normal text-[20px] ">Most calendars are designed for teams. Slate is designed for 
+            <h1 className="mt-10 p-3 font-regular md:font-medium text-[80px] w-[420px] md:w-[700px]">Work at the speed of thought</h1>
+            <p className="my-5 w-[380px] md:w-[600px] font-normal text-[20px] ">Most calendars are designed for teams. Slate is designed for 
             freelancers who want a simple way to plan their schedule.</p>
-            <div className="flex mt-[70px] mb-3">
-                <a href="#"  className="font-medium mr-20">
+            <div className="flex flex-col md:flex-row mt-[70px] mb-3">
+                <a href="#"  className="font-medium mb-6 md:mr-20">
                 <button type="button" className="btn w-44 h-16 bg-[#4452FE] text-white py-2 hover:text-[#4452FE] hover:bg-gray-200">Try For Free</button>
                 </a>
                 <a href="#" className="font-medium">
