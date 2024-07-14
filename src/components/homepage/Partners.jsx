a45781feb7a582f10e3e396f44187f2b03c4d373
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 23:39:45 +0530

    Responsive

--- src/components/homepage/Partners.jsx
+++ src/components/homepage/Partners.jsx
@@ -6,14 +6,17 @@ function Partners(){
             <Title
             title="Partners"
             description="We focus on ergonomics and meeting you where you work. It's only a keystroke away."/>
-            <div className="flex justify-evenly m-[40px] w-[900px]">
-                <img src="src\assets\logos_apple-app-store.png"></img>
-                <img src="src\assets\logos_apiary.png"></img>
-                <img src="src\assets\logos_android-icon.png"></img>
-                <img src="src\assets\logos_basecamp.png"></img>
-                <img src="src\assets\logos_airbnb.png"></img>
-                <img src="src\assets\logos_ibm.png"></img>
+            <div className="m-[40px] w-[60%]">
+                <div className="grid grid-cols-3 md:grid-cols-6 gap-4 place-items-center">
+                    <img src="src/assets/logos_apple-app-store.png" alt="Apple" />
+                    <img src="src/assets/logos_apiary.png" alt="Apiary" />
+                    <img src="src/assets/logos_android-icon.png" alt="Android Icon" />
+                    <img src="src/assets/logos_basecamp.png" alt="Basecamp" />
+                    <img src="src/assets/logos_airbnb.png" alt="Airbnb" />
+                    <img src="src/assets/logos_ibm.png" alt="IBM" />
+                </div>
             </div>
+
             <a href="#" className="font-medium m-5">
                 <button type="button" className="btn w-44 h-12 text-white py-2 border border-solid border-white hover:text-black hover:bg-gray-200">All Partners</button>
             </a>
--- src/components/homepage/Testimonial.jsx
+++ src/components/homepage/Testimonial.jsx
@@ -3,71 +3,67 @@ import Title from "./Title";
 function Testimonial(){
     const Person = ({photo,names,role,text}) =>(
         <div className="border border-white rounded-lg flex flex-col justify-between items-center m-2 p-8">
-            <div className="flex justify-start w-full ">
-                <img src={photo}></img>
+            <div className="flex justify-start w-full p-4">
+                <img src={photo} alt={names}/>
                 <div className="font-medium text-white text-[16px] ml-2">
                     <h6>{names}</h6>
-                    <p >{role}</p>
+                    <p>{role}</p>
                 </div>
             </div>
-            <p className="w-72 text-white text-left">{text}</p>
+        <p className="w-72 text-white text-left">{text}</p>
         </div>
     );
     return(
         <div className="flex flex-col items-left">
             <Title
             title="Testimonials"/>
-            <div className="flex justify-center mt-8">
-                <Person
-                    photo="src\assets\t1.png"
-                    names="Claire Bell"
-                    role="Designer"
-                    text="Slate helps you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year. Slate helps 
-                        you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year.your financial 
-                        goal for the month and year."
+            <div className="flex flex-colg items-center">
+                <div className="w-fit grid grid-cols-1 md:grid-cols-2 gap-2 mt-8">
+                    <Person
+                        photo="src/assets/t1.png"
+                        names="Claire Bell"
+                        role="Designer"
+                        text="Slate helps you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year. Slate helps
+                            you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year."
                     />
                     <Person
-                    photo="src\assets\t2.png"
-                    names="Francisco Lane"
-                    role="Designer"
-                    text="Slate helps you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year. Slate helps 
-                        you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year.your financial 
-                        goal for the month and year."
-                    />
-            </div>
-            <div className="flex justify-center">
-            <Person
-                    photo="src\assets\t3.png"
-                    names="Ralph Fisher"
-                    role="Designer"
-                    text="Slate helps you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year. Slate helps 
-                        you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year.your financial 
-                        goal for the month and year."
+                        photo="src/assets/t2.png"
+                        names="Francisco Lane"
+                        role="Designer"
+                        text="Slate helps you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year. Slate helps
+                            you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year."
                     />
                     <Person
-                    photo="src\assets\t4.png"
-                    names="Jorge Murphy"
-                    role="Designer"
-                    text="Slate helps you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year. Slate helps 
-                        you see how many more days 
-                        you need to work to reach your financial 
-                        goal for the month and year.your financial 
-                        goal for the month and year."
+                        photo="src/assets/t3.png"
+                        names="Ralph Fisher"
+                        role="Designer"
+                        text="Slate helps you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year. Slate helps
+                            you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year."
                     />
+                    <Person
+                        photo="src/assets/t4.png"
+                        names="Jorge Murphy"
+                        role="Designer"
+                        text="Slate helps you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year. Slate helps
+                            you see how many more days
+                            you need to work to reach your financial
+                            goal for the month and year."
+                    />
+                </div>
             </div>
         </div>
     )
