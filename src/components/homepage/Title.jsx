56a468a4e7cc0340ebc283c3fa3a91fa4a12ad15
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 22:33:53 +0530

    Responsive title

--- src/components/homepage/Title.jsx
+++ src/components/homepage/Title.jsx
@@ -2,7 +2,7 @@ function Title({ title, description }) {
     return (
         <div className="h-auto mt-10 flex flex-col items-center justify-center text-white text-center">
             <h2 className="p-5 font-normal text-[52px]">{title}</h2>
-            <p className="p-2 w-[600px] font-normal text-[20px] ">{description}</p>
+            <p className="p-2 my-5 w-[380px] md:w-[600px] font-normal text-[20px] ">{description}</p>
         </div>
     );
 }
