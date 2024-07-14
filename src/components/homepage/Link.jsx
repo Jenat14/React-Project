e7336a167defae35cc133b25bea9eaa0e0de1e8e
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-14 13:21:43 +0530

    Link.jsx completed

--- src/components/homepage/Link.jsx
+++ src/components/homepage/Link.jsx
@@ -1,7 +1,20 @@
+import Title from "./Title";
+
 function Link(){
     return(
-        <div className="bg-[#343434] align-center w-[1200px] h-[380px]">
-
+        <div className="flex justify-center items-center">
+            <div className="bg-[#343434] w-[1500px] flex h-[380px] rounded-xl m-28">
+                <div className="p-5 w-6/12">
+                    <Title
+                    title="OpenType features and Variable fonts"/>
+                    <a href="#"  className="font-medium">
+                <button type="button" className="btn w-60 h-14 bg-[#4452FE] text-white ml-5 hover:bg-white hover:text-[#4452FE]">Try For Free</button>
+                </a>
+                </div>
+                <div className="content-end">
+                    <img src="src\assets\template.png"></img>
+                </div>
+            </div>
         </div>
     )
 }
