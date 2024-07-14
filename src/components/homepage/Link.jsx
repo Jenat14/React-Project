16095933d4003381291b9ba70ab7d3544add123d
Jenat Cici Anil <133338629+Jenat14@users.noreply.github.com>
2024-07-15 00:09:27 +0530

    Styling

--- src/components/homepage/Link.jsx
+++ src/components/homepage/Link.jsx
@@ -3,13 +3,12 @@ import Title from "./Title";
 function Link(){
     return(
         <div className="flex items-center">
-            <div className="bg-[#343434] w-[1500px] flex flex-col md:flex-row h-[380px] rounded-xl m-28">
-                <div className="p-4 w-6/12">
-                    <Title
-                    title="OpenType features and Variable fonts"/>
+            <div className="bg-[#343434] w-[1600px] flex flex-col items-center lg:flex-row h-auto rounded-xl m-28">
+                <div className="p-0 md:p-7 w-8/12 md:w-6/12 items-center">
+                    <h2 className="text-white text-center mb-4 font-normal text-[52px]">OpenType features and Variable fonts</h2>
                     <a href="#"  className="font-medium">
-                <button type="button" className="btn w-60 h-14 bg-[#4452FE] text-white ml-5 hover:bg-white hover:text-[#4452FE]">Try For Free</button>
-                </a>
+                        <button type="button" className="btn w-60 h-14 bg-[#4452FE] text-white hover:bg-white hover:text-[#4452FE]">Try For Free</button>
+                    </a>
                 </div>
                 <div>
                     <img src="src\assets\template.png" className="h-full"></img>
