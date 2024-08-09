9c302513e216bc645d0721e94f017d910e53fc1c
Jenat14 <Jenat14@users.noreply.github.com>
2024-08-09 11:15:27 +0000

    deploy: 5330b9d11c4550b992bbec96f4ecbd931e1de99e

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Web Interface
-
-web interface developed with react along eith vite. Styling done with tailwind css
--- src/components/homepage/Photo.jsx
+++
@@ -1,9 +0,0 @@
-function Photo(){
-    return(
-        <div className="relative mt-4 mb-40">
-            <img src="src\assets\i-bg.png" className="z-index[-10]"></img>
-            <div class="absolute top-20 left-0 w-full h-full bg-gradient-to-b from-transparent to-black"></div>
-        </div>
-    )
-}
-export default Photo;
